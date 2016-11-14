8eb8200f0eb759950543d8209fe4c46c1ce206cf
xwh16 <xwh16@hotmail.com>
2016-11-14 12:54:45 +0800

    直接向重定向流中写入request.content，POST脚本成功执行

---
+++ HttpServer/HttpServer/ResourceHandlers/cgiHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServer.HttpServer
+{
+    public class cgiHandler
+    {
+        private Dictionary<string, string> cgiPath;
+        
+    }
+}
